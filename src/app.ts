7080b7662e282ea6aed48dd38eed09228038b899
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-02-26 14:48:56 -0300

    refactor: upload route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import express from "express";
+import multer from "multer";
 
 export const app = express();
+const upload = multer();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    try {
+        res.send('File was uploaded with success: ' + req.file.originalname);
+    } catch (error) {
+        console.log(error);
+    }
+   
+});
