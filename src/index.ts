a43b7d60e77c405a82cb95bae7d5656c64f31937
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-02-26 13:13:25 -0300

    refactor: move code index.ts to app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import express from 'express';
-import multer from 'multer';
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+import { app } from "./app";
 
 app.listen(3000, () => console.log('listening on port 3000'));
